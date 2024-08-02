4e3d85be29761858455b3bf63f06543e605f1f32
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-02 22:03:30 +0200

    randomised the animal type being spawned with Random.Range

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    
     
     // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
+            var animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(
                 animalPrefabs[animalIndex], 
                 new Vector3(0, 0, 19.9f),
