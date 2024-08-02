7510a9cf1a3b534d5b495550edff0be2b2819040
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-02 21:32:09 +0200

    implemented another bounds for destroying objects on the lower bound of the screen

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     public float topBounds = 20.0f;
+    public float lowerBounds = -10.0f;
 
     // Update is called once per frame
     private void Update()
     {
         if (transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBounds)
         {
             Destroy(gameObject);
         }
