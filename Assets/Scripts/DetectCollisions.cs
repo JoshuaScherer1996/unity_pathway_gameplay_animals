59005ae3ab60aa1408084196b9898f0029d15ccb
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-06 15:19:04 +0200

    added the script DetectCollisions to control object destruction on collision

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
